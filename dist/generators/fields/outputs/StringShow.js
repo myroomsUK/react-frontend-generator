6b83379df9a8d12755c59c6be61be851cb82a4b9
jacopo <jacopotrapani89@gmail.com>
2021-05-19 13:44:05 +0200

    fix nested show 2

--- dist/generators/fields/outputs/StringShow.js
+++ dist/generators/fields/outputs/StringShow.js
@@ -1,5 +1,4 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 export default function StringShow({ propertyRecord }) {
-    console.log("WHAAT", propertyRecord);
     return _jsx("div", { children: propertyRecord }, void 0);
 }
