33b25d343a1bd59ab4e8d4abd7134dc9410e06b2
jacopo <jacopotrapani89@gmail.com>
2021-05-26 10:08:18 +0200

    fix reference show

--- dist/generators/fields/outputs/ReferenceShow.d.ts
+++ dist/generators/fields/outputs/ReferenceShow.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="react" />
-export default function ReferenceShow({ propertyModel, propertyRecord }: any): JSX.Element;
+import { ReferenceModel } from "../../../resource-models/propertyModels/ReferenceModel";
+export default function ReferenceShow({ propertyModel, propertyRecord }: ReferenceOutput): JSX.Element;
+interface ReferenceOutput {
+    propertyModel: ReferenceModel;
+    propertyRecord: Map<string, any> | number;
+}
+export {};
