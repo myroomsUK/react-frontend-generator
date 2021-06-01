d59a2bdf498eb7e95ce5e7165c87e897a4a43b13
jacopo <jacopotrapani89@gmail.com>
2021-06-01 10:47:53 +0200

    reference show fixed

--- src/generators/fields/outputs/ReferenceShow.tsx
+++ src/generators/fields/outputs/ReferenceShow.tsx
@@ -5,8 +5,10 @@ import {ReferenceModel} from "../../../resource-models/propertyModels/ReferenceM
 export default function ReferenceShow({propertyModel,propertyRecord}:ReferenceOutput){
     console.log("propertyRecord", propertyRecord)
     if(propertyRecord){
-        const id = (typeof propertyRecord==="number") ? propertyRecord : propertyRecord.get("id")
-        const name = (typeof propertyRecord==="number") ? propertyRecord : propertyRecord.get(propertyModel.optionText)
+        // @ts-ignore
+        const id = (typeof propertyRecord==="number") ? propertyRecord : propertyRecord["id"]
+        // @ts-ignore
+        const name = (typeof propertyRecord==="number") ? propertyRecord : propertyRecord[propertyModel.optionText]
         return <Link color="secondary" href={`/${propertyModel.resourceName}/${id}/show`}>{name}</Link>
     }
     return <></>
