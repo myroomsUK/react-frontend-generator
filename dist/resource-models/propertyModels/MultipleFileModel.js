f2b586d11c4745b9f09060d8b13eafd7e4d5ee0b
jacopo <jacopotrapani89@gmail.com>
2021-07-02 15:14:57 +0200

    fix delete file

--- dist/resource-models/propertyModels/MultipleFileModel.js
+++ dist/resource-models/propertyModels/MultipleFileModel.js
@@ -6,7 +6,7 @@ export class MultipleFileModel extends SinglePropertyModel {
     setInputField(props) {
         const { formValue, setFormValue, errors } = props;
         // @ts-ignore
-        const propsWithModel = Object.assign(Object.assign({}, props), { model: this, modelResourceName: this.modelResourceName, onChange: this.getInputOnChangeHandler({ formValue, setFormValue }), files: formValue[this.id] });
+        const propsWithModel = Object.assign(Object.assign({}, props), { model: this, resourceId: formValue.id, modelResourceName: this.modelResourceName, onChange: this.getInputOnChangeHandler({ formValue, setFormValue }), files: formValue[this.id] });
         // @ts-ignore
         return FileListInput(propsWithModel);
     }
