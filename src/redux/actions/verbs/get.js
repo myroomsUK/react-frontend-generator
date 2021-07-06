2cb102c9ac09c1592babfa4516d5b743b5e08136
jacopo <jacopotrapani89@gmail.com>
2021-07-06 11:24:37 +0200

    get function

---
+++ src/redux/actions/verbs/get.js
@@ -0,0 +1,42 @@
+import {fetch} from '../dataAccess';
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+
+export function error(error) {
+    return { type: 'GET_ERROR', error };
+}
+
+export function loading(loading) {
+    return { type: 'GET_LOADING', loading };
+}
+
+export function success(retrieved) {
+    return { type: 'GET_SUCCESS', retrieved };
+}
+
+export function useGet() {
+
+    const [data, setData] = useState();
+    const [errors, setErrors] = useState({});
+    const dispatch = useDispatch();
+
+    const get = (route) => {
+        setErrors({});
+        return fetch(`${route}`)
+            .then(response => response.json())
+            .then(retrieved => ({ retrieved }))
+            .then(({ retrieved, hubURL }) => {
+                dispatch(loading(false));
+                dispatch(success(retrieved));
+                setData(retrieved);
+                return retrieved
+            })
+            .catch(e => {
+                dispatch(loading(false));
+                dispatch(error(e.message));
+                setErrors(e.errors);
+                throw new Error(e.message())
+            });
+    }
+    return {data, get};
+}
